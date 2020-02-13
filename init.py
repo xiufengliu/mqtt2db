8be004b89bc52649413cbadc84b5b29be5609a9a
groupme@gmail.com <groupme@gmail.com>
2020-02-13 21:26:02 +0100

    add files

---
+++ init.py
@@ -0,0 +1,50 @@
+import psycopg2
+from config import config
+
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        drop table if exists DHT22_Temperature_Data
+        """,
+        """ create table DHT22_Temperature_Data (
+              id integer primary key autoincrement,
+              SensorID text,
+              Date_n_Time text,
+              Temperature text
+            )
+        """,
+        """
+       drop table if exists DHT22_Humidity_Data 
+        """,
+        """
+        create table DHT22_Humidity_Data (
+              id integer primary key autoincrement,
+              SensorID text,
+              Date_n_Time text,
+              Humidity text
+            )
+        """)
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
